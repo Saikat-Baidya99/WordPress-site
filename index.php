8935e21c9b4efae371663fca7afccb7ee9c46870
Saikat Baidya <113163609+Saikat-Baidya99@users.noreply.github.com>
2023-11-24 01:05:46 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title></title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
+ </head>
+ <body>
+     <header class="site-headers">
+         <nav>
+             <div class="logo">
+            <h1>Modern Website</h1>
+             </div>
+             <div class="menu">
+             <ul>
+                 <li><a href="">home</a></li>
+                 <li><a href="">servics</a></li>
+                 <li><a href="">aboutus</a></li>
+                 <li><a href="">contactus</a></li>
+             </ul>
+             </div>
+         </nav>
+         <section>
+             <div class="leftside">
+                 <img src="<?php echo get_template_directory_uri();?>/free.jpg">
+             </div>
+             <div class="rightside">
+                <h1> our mordern website</h1>
+                <p> We Are The Best Website Developer Comapany.</p>
+                <button> wellcome</button>
+             </div>
+         </section>
+     </header>
+
+ </body>
+</html>
